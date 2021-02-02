26a08bf99b9fc2da6ca98514e6eb7572a639acee
Gary Desktop <frublob46@gmail.com>
2021-02-02 14:23:26 +0000

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,9 @@ import 'lazysizes' /* doesn't need to be instantiated */
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
 import StickyHeader from './modules/StickyHeader.js'
+import ClientArea from './modules/ClientArea.js'
 
+new ClientArea
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
